37d39714fefe96fc6a33bf4fa1a0a6e12512c3a3
Radia731 <radiajjr17@gmail.com>
2024-10-05 21:32:39 -0400

    labwork

---
+++ Module 8 - Node-Express/book-api/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+const books = require ('./books');
+
+app.use(express.json());
+
+
+app.get('/api/books', (req, res) => {
+
+    res.json(books);
+});
+
+app.listen(port, () => {
+    console.log(`sever is running on ${port}`)
+})
