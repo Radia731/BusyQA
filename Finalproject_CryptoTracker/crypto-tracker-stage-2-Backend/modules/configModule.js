f7c5b21149d08432316ec3097a5f69286a72423e
Radia731 <radiajjr17@gmail.com>
2024-10-23 01:28:47 -0400

    fixed the CORS error and made the external api call to be performed by the backend

---
+++ Finalproject_CryptoTracker/crypto-tracker-stage-2-Backend/modules/configModule.js
@@ -0,0 +1,7 @@
+const coinMarketCapApiKey = 'e4d176e0-adfb-41ab-82d4-f67a95893eac';
+const coinMarketCapApiurl = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest';
+
+module.exports = {
+     coinMarketCapApiKey, 
+     coinMarketCapApiurl
+     };
