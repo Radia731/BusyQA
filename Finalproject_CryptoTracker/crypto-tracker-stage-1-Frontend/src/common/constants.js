ed0d655c7fa2bbf484b7e6101aa4a2fa1111506b
Radia731 <radiajjr17@gmail.com>
2024-10-23 00:13:05 -0400

    fixed api end point to add and render watchList coins

---
+++ Finalproject_CryptoTracker/crypto-tracker-stage-1-Frontend/src/common/constants.js
@@ -0,0 +1,5 @@
+// src/common/constants.js
+const coinMarketCapApiKey = 'e4d176e0-adfb-41ab-82d4-f67a95893eac';
+const coinMarketCapApiurl = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest';
+
+export { coinMarketCapApiKey, coinMarketCapApiurl }; // Correct export
