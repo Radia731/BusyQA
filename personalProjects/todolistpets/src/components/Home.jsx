eb37e1e7cd8b78804f6857a53c1eefad36e1127b
Radia731 <radiajjr17@gmail.com>
2024-10-31 02:17:11 -0400

    new project- created a responsive home component

---
+++ personalProjects/todolistpets/src/components/Home.jsx
@@ -0,0 +1,30 @@
+
+function  Home() {
+   
+    return(
+       
+        <div className="WelcomeBg">
+        <h1>Rad's Pet Duty!</h1> 
+        <div className="pet-card">
+            <div className="Dog-card">
+              <img src="https://www.pngall.com/wp-content/uploads/10/Pet-PNG-Images.png" alt="mrweston" />
+              <h2> Weston</h2>
+            </div>
+            <div className="cat-card">
+              <img src="https://stickershop.line-scdn.net/stickershop/v1/product/23701012/LINEStorePC/main.png?v=1" alt="missgowrl" />
+              <h2> Aliya</h2>
+            </div>
+            <div className="bunny-card">
+              <img src="https://freepngimg.com/thumb/rabbit/7-2-rabbit-png-picture.png" alt="mrweston"  />
+              <h2> Tpain</h2>
+            </div>
+        
+            
+        </div>
+
+        </div>
+    )
+
+}
+
+export default Home;
